73d7c557a40c8ba164141d6b2d94df3da95e67b1
PRSMendis <rayhan.mendis@gmail.com>
2022-07-13 15:25:19 +1000

    shouldn't be closest number, should be closest number that is lower than the current number

--- main.py
+++ main.py
@@ -8,16 +8,30 @@ tax_dict = collections.OrderedDict()
 # tax_dict = {2020: 'Hi', 2021: 'Hello'}
 
 # Might need to bump up index vals up by 1 due to binary search
-tax_dict = {2020:  {
-    0: 1,
-    18201: {
-        "threshold": 18200,
-        "rate": 0.19
-    },
-    45001: 3,
-    120001: 4,
-    180001: 5
-}}
+tax_dict = {
+    2020:  {
+        18201: {
+            "lump": 0,
+            "threshold": 18200,
+            "rate": 0.19
+        },
+        45001: {
+            "lump": 5092,
+            "threshold": 45000,
+            "rate": 0.325
+        },
+        120001: {
+            "lump": 29467,
+            "threshold": 120000,
+            "rate": 0.37
+        },
+        180001: {
+            "lump": 51667,
+            "threshold": 180000,
+            "rate": 0.45
+        }
+    }
+}
 str_map = {"2020-21": 2020, "2021-22": 2021}
 
 
@@ -41,15 +55,26 @@ def main():
     # idx = bisect.bisect_left(
     #     list(tax_dict[str_map[options[menu_entry_index]]].keys()), income)
 
-    search_key = income
+    if income <= 18200:
+        return 0
+
     income_dict = tax_dict[str_map[options[menu_entry_index]]]
-    res = income_dict.get(search_key) or income_dict[
-        min(income_dict.keys(), key=lambda key: abs(key-search_key))]
 
-    print("res", res)
+    rates = income_dict.get(income) or income_dict[
+        min(income_dict.keys(), key=lambda key: abs(key-income))]
 
-    print(tax_dict[2020])
+    print("income_dict.keys()", income_dict.keys())
+    print("min(income_dict.keys(), key=lambda key: abs(key-income))",
+          min(income_dict.keys(), key=lambda key: key-income))
 
+    print("res", rates)
+
+    income_tax = rates["lump"] + \
+        ((income - rates["threshold"]) * rates["rate"])
+
+    print(income_tax)
+
+    return income_tax
 
     # sorted(list_of_numbers, key=lambda x: abs(x - number))[0]
 if __name__ == "__main__":
