b7584095dbf1ef34e2fc045d0e788f50a3ea2022
PRSMendis <rayhan.mendis@gmail.com>
2022-07-13 14:57:38 +1000

    no longer using binary search

--- main.py
+++ main.py
@@ -6,9 +6,14 @@ from simple_term_menu import TerminalMenu
 # initializing dictionary
 tax_dict = collections.OrderedDict()
 # tax_dict = {2020: 'Hi', 2021: 'Hello'}
+
+# Might need to bump up index vals up by 1 due to binary search
 tax_dict = {2020:  {
     0: 1,
-    18201: 2,
+    18201: {
+        "threshold": 18200,
+        "rate": 0.19
+    },
     45001: 3,
     120001: 4,
     180001: 5
@@ -31,10 +36,20 @@ def main():
 
     print(income)
 
-    res = bisect.bisect_left(
-        list(tax_dict[str_map[options[menu_entry_index]]].keys()), income)
+    # Search through dict and find appropriate tax bracket key
 
-    print(res)
+    # idx = bisect.bisect_left(
+    #     list(tax_dict[str_map[options[menu_entry_index]]].keys()), income)
+
+    search_key = income
+    income_dict = tax_dict[str_map[options[menu_entry_index]]]
+    res = income_dict.get(search_key) or income_dict[
+        min(income_dict.keys(), key=lambda key: abs(key-search_key))]
+
+    print("res", res)
+
+    print(tax_dict[2020])
+
 
     # sorted(list_of_numbers, key=lambda x: abs(x - number))[0]
 if __name__ == "__main__":
