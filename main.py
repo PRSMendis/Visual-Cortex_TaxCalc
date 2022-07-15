191ce72d80955954130aaa55ffc7b43d4579393a
PRSMendis <rayhan.mendis@gmail.com>
2022-07-15 11:23:37 +1000

    refactoring logic

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import collections
 from simple_term_menu import TerminalMenu
 
+# python version 3.19.12
 
 # tax_dict = collections.OrderedDict()
 tax_dict = {
@@ -55,28 +56,25 @@ str_map = {"2020-21": 2020, "2021-22": 2021}
 def user_input() -> float:
     while True:
         try:
-            return float(input("Please enter your total taxable income for the full income year::\n"))
+            val = float(
+                input("Please enter your total taxable income for the full income year:\n"))
+            if val < 0:
+                print("Sorry, input must be a positive value, try again")
+                continue
+            else:
+                return val
         except ValueError:
             print("Input only accepts numbers.")
 
 
-def main(tax_dict, str_map):
-    print("Please select the income year from the following: ")
-    # Choose the tax year range from options, and map to float value
+def calculator(income, year_dict):
 
-    options = ["2020-21", "2021-22"]
-    terminal_menu = TerminalMenu(options)
-    menu_entry_index = terminal_menu.show()
-    income = user_input()
-    # Search through dict and find appropriate tax bracket key
-    # idx = bisect.bisect_left(
-    #     list(tax_dict[str_map[options[menu_entry_index]]].keys()), income)
+    min_tax_threshold = min(year_dict.keys())
 
-    if income <= 18200:
+    if income < min_tax_threshold:
         print(f"The estimated tax on your taxable income is: $0")
     else:
         # access the year subcollection
-        year_dict = tax_dict[str_map[options[menu_entry_index]]]
 
         # access the rates that affect the user based on input number
         rates = year_dict.get(income) or year_dict[
@@ -88,6 +86,22 @@ def main(tax_dict, str_map):
 
         print(
             f"The estimated tax on your taxable income is: ${'{:,.2f}'.format(income_tax)}")
+
+
+def main(tax_dict, str_map):
+    print("Please select the income year from the following: ")
+    # Choose the tax year range from options, and map to float value
+
+    options = ["2020-21", "2021-22"]
+    terminal_menu = TerminalMenu(options)
+    menu_entry_index = terminal_menu.show()
+    income = user_input()
+    year_dict = tax_dict[str_map[options[menu_entry_index]]]
+    calculator(income=income, year_dict=year_dict)
+
+    # Search through dict and find appropriate tax bracket key
+    # idx = bisect.bisect_left(
+    #     list(tax_dict[str_map[options[menu_entry_index]]].keys()), income)
 
 
 if __name__ == "__main__":
